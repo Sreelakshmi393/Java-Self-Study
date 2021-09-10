7c335dd51fb991b62a71ddfc4a35d31e82808e3a
Sreelakshmi Anilkumar <sreelakshmita2015@gmail.com>
2021-09-10 19:03:40 +0530

    More commits

---
+++ code/src/com/company/Prime.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+    }
+    static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        int c =2;
+        while (c*c <= n){
+            if (n%c==0){
+                return false;
+            }
+            c++;
+        }
+        return c*c > n;
+    }
+}
